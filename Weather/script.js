ac4ce07042e446030e057ba2b02140ca3ca89dce
kkc04 <kalyanichavan626@gmail.com>
2024-06-19 21:01:22 +0530

    Update script.js

--- Weather/script.js
+++ Weather/script.js
@@ -1,62 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = "39a03ee093bfba064223e354cb3110f5";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    const SubmitBox = document.querySelector("#submitText");
+    const SubmitBtn = document.querySelector("#submitOption");
+    const weatherIcon = document.querySelector(".weather-icon");
 
-const apiKey = "39a03ee093bfba064223e354cb3110f5";
-const apiUrl = "https://pro.openweathermap.org/data/2.5/forecast/climate?units=metric";
+    async function FetchWeather(city) {
+        try {
+            const response = await fetch(apiUrl + city +`&units=metric`+`&appid=${apiKey}` );
+            if (response.status === 404) {
+                document.querySelector(".error").style.display = "block";
+                document.querySelector(".weather").style.display = "none";
+            } 
+            
+            else {
+                const data = await response.json();
+                console.log(data);
+                document.querySelector(".city").innerHTML = data.name;
+                document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+                document.querySelector(".humidity p").innerHTML = data.main.humidity + "%";
+                document.querySelector(".Wind p").innerHTML = data.wind.speed + " km/hr";
 
-const SubmitBox = document.querySelector(".Navbar input");
-const SubmitBtn= document.querySelector(".Navbar button");
-const weatherIcon = document.querySelector(".weather-icon");
-
-async function FetchWeather(city) {
-
-
-    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
-    
-    if(response.status==404){
-        document.querySelector(".error").style.display="block";
-        document.querySelector(".weather").style.display="none";
+                switch (data.weather[0].main) {
+                    case "Clouds":
+                        weatherIcon.src = "cloudy.jpeg";
+                        break;
+                    case "Clear":
+                        weatherIcon.src = "clear.png";
+                        break;
+                    case "Rain":
+                        weatherIcon.src = "rainy.webp";
+                        break;
+                    case "Drizzle":
+                        weatherIcon.src = "rain&thunder.webp";
+                        break;
+                    case "Mist":
+                        weatherIcon.src = "mist.jpeg";
+                        break;
+                }
+                document.querySelector(".weather").style.display = "block";
+                document.querySelector(".error").style.display = "none";
+            }
+        } catch (error) {
+            console.error("Error fetching weather data: ", error);
+        }
     }
-    else{
-    var data = await response.json();//will fetch the data from json
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".Wind").innerHTML = data.wind.speed + " km/hr";
 
-    if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "cloudy.jpeg";
-
-    }
-
-    else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "clear.png";
-
-    }
-
-
-    else if (data.weather[0].main == "Rain") {
-        weatherIcon.src = "rainy.webp";
-
-    }
-
-    else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "rain&thunder.webp";
-
-    }
-
-    else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "mist.jpeg";
-
-    }
-    document.querySelector(".weather").style.display="block";
-    document.querySelector(".error").style.display="none";
-}
-
-}
-
-SubmitBtn.addEventListener("click", ()=>{
-    FetchWeather(SubmitBox.value);
-})
-
-
-
+    SubmitBtn.addEventListener("click", () => {
+        FetchWeather(SubmitBox.value);
+    });
+});
